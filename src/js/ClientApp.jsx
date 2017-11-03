3df044171478f8114c93a28518970d389d463a8f
NYCSwan <megan.swanby@gmail.com>
2017-11-03 17:02:10 -0400

    fixed 404 route

--- src/js/ClientApp.jsx
+++ src/js/ClientApp.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Homepage from './Homepage.react';
 import NewGrow from './new_grow.react';
+import ExistingGrow from './existing_grow.react';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -13,6 +14,8 @@ const App = () => (
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/newgrow" component={NewGrow} />
+          <Route exact path="/existinggrow" component={ExistingGrow} />
+
           <Route component={FourOhFour} />
         </Switch>
       </div>
