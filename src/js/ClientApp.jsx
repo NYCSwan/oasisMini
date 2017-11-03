f4ea28bef7b73d6f20e2b04898036b4412b23e3e
NYCSwan <megan.swanby@gmail.com>
2017-11-03 15:04:31 -0400

    Add divs and dev server command to package.json, fix babel error

--- src/js/ClientApp.jsx
+++ src/js/ClientApp.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+
 import Header from './Header.react';
 
-const App = (props) => (
-    <div className="app">
-      <Header title='Oasis Mini Garden' />
-    </div>
+const App = () => (
+    <HashRouter>
+        <div className="app">
+          <Route path="/" component={Header} />
+          <div>New Grow</div>
+          <div>Existing Grow</div>
+          <div>tutorials</div>
+        </div>
+    </HashRouter>
 )
 
-render(<App />, document.getElementById('app'))
+render(<App />, document.getElementById('app'));
--- src/js/Header.react.jsx
+++ src/js/Header.react.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
     <div className="landing">
-       <h1>{props.title}</h1>
+       <h1>title</h1>
      </div>
 )
 
