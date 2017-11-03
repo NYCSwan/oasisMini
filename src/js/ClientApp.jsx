c5d85fb38230360e0c4c072b3468c48926e9b846
NYCSwan <megan.swanby@gmail.com>
2017-11-03 13:23:51 -0400

    Add Header, fix gitignore for yarn-errors

--- src/js/ClientApp.jsx
+++ src/js/ClientApp.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Header from './Header.react';
 
 const App = (props) => (
     <div className="app">
-      <div className="landing">
-        <h1>Oasis Mini Homepage</h1>
-      </div>
+      <Header title='Oasis Mini Garden' />
     </div>
 )
 
---
+++ src/js/Header.react.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="landing">
+       <h1>{props.title}</h1>
+     </div>
+)
+
+export default Header;
