046b12ce53d495b2c9cdb5308101ea8e180a403f
NYCSwan <megan.swanby@gmail.com>
2017-11-07 19:29:11 -0500

    break out graphs into child container

---
+++ src/js/graph.react.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Graph = props => (
+  <h3>{props.data}</h3>
+)
+
+export default Graph;
--- src/js/monitor.react.jsx
+++ src/js/monitor.react.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import preload from '../../data.json';
 import Header from './Header.react';
-import MonitoredDataPoint from './monitored_data_point.react'
+import Graph from './graph.react';
 
 const Monitor = () => (
   <div className="monitor">
@@ -9,21 +9,21 @@ const Monitor = () => (
     <h1>Monitor</h1>
     <div className="container">
       <div>filter function by chamber</div>
-    <div className="D3Graph temperature">
-      {preload.sensor_data.map( data => (
-        <h3>{data.temperature}</h3>
-      ))}
-    </div>
     <div className="D3Graph humidity">
       {preload.sensor_data.map( data => (
-        <h3>{data.humidity}</h3>
+        <Graph key={data.time} data={data.humidity} />
       ))}
     </div>
     <div className="d3Graph height">
       {preload.sensor_data.map( data => (
-        <h3>{data.height}</h3>
+        <Graph key={data.time} data={data.height} />
       ))}
-    </div>*/}
+    </div> 
+     <div className="d3graph temperature">
+      {preload.sensor_data.map( data => (
+         <Graph key={data.time} data={data.temperature} />
+       ))}
+     </div>
     <div className="pH">
       {preload.sensor_data.map(data => <h3>pH: {data.pH}</h3>)}
     </div>
